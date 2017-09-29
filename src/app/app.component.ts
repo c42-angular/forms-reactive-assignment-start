732725f06eed9f29838ffe087c74281b4b402b59
arun@win10.bootcamp.com <arun@win10.bootcamp.com>
2017-09-29 16:28:38 +0100

    project name - required input implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  theForm: FormGroup;
+
+  onFormSubmit() {
+    console.log(this.theForm);
+  }
+
+  ngOnInit(): void {
+    this.theForm = new FormGroup({
+      'projectName': new FormControl(null, Validators.required),
+      'projectMail': new FormControl(null),
+      'projectStatus': new FormControl(null)
+    });
+  }
 }
